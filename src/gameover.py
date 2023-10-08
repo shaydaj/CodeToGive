2b4b4bc461da1d544dc88887d43efb0f667a87e0
Muhammad Hamza Asim <Muhammad.asim-2@student.manchester.ac.uk>
2023-10-08 13:44:19 +0100

    changed gameover slightly

--- src/gameover.py
+++ src/gameover.py
@@ -6,19 +6,21 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 960, 720
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 WHITE = (255, 255, 255)
 
-
 background_image = pygame.image.load('assets/images/endingbackground.jpg')
 background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
 gameover = pygame.image.load('assets/images/gameover.png')
 gameoverimage = pygame.transform.scale(gameover, (575, 175))
-gameover_rect = gameoverimage.get_rect(center=(SCREEN_WIDTH // 2 , SCREEN_HEIGHT // 2 - 100))
+gameover_rect = gameoverimage.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 100))
 
 playagain = pygame.image.load('assets/images/playagainbutton.png')
 playagainimage = pygame.transform.scale(playagain, (200, 100))
 
-playagainrect = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 , 320, 120)
+playagainrect = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2, 320, 120)
+
+
+def new_screen():
+    pass
 
 
 running = True
@@ -31,13 +33,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     screen.blit(gameoverimage, gameover_rect)
 
     if playagainrect.collidepoint(mouse_pos):
         screen.blit(pygame.transform.scale(playagainimage, (220, 120)), playagainrect.move(-10, -5))
     else:
         screen.blit(playagainimage, playagainrect)
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        if playagainrect.collidepoint(mouse_pos):
+            new_screen()
 
     pygame.display.flip()
 
